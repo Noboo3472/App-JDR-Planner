d33dcea52a495a27ec2e91e5ced3f9fd977b1f38
Noboo <noboo.pro@gmail.com>
2026-06-29 10:48:52 +0200

    Initialisation Backend : NodeJs+Express + installation et configuration de Prisma pour un environnement JS

---
+++ backend/lib/prisma.js
@@ -0,0 +1,16 @@
+// lib/prisma.js
+import { PrismaClient } from '../src/generated/index.js';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL || 'file:./dev.db',
+});
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
